889cc5276fcbe1e40ddeb8085d1c091332710b62
HurtowskyYT <michoorangutan@gmail.com>
2023-11-16 12:59:45 +0100

    Added guildMemberRemove.js

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,15 +1,15 @@
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    name: "guildMemberAdd",
+    name: "guildMemberRemove",
     execute(member) {
         const { user, guild } = member;
         const welcomeChannel = member.guild.channels.cache.get('1159608455125020742');
 
         const byeEmbed = new EmbedBuilder()
-            .setColor('#00ff00')
+            .setColor('#ff0000')
             .setAuthor({name: 'User Left!', iconURL: member.user.displayAvatarURL()})
-            .setDescription(`${member.name} left.\nWe now have [\`${guild.memberCount}\`] members!`)
+            .setDescription(`<@${member.id}> left.\nWe now have [\`${guild.memberCount}\`] members!`)
             .setTimestamp();
 
         welcomeChannel.send({ embeds: [byeEmbed] });
