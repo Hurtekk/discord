f3c40d43a7ac951707ba33c513dffa92a5be76a7
HurtowskyYT <michoorangutan@gmail.com>
2023-11-17 10:16:05 +0100

    Changed ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,6 +4,6 @@ module.exports = {
     name: "ready",
     once: true,
     execute(client) {
-        console.log(`${client.user.username} is now online.`);
+        console.log(`${client.user.tag} is now online.`);
     }
 }
