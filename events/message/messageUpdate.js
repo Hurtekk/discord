8c764b4764acdf4864da62bc8a9d976f70248f64
Michał Kulik <michoorangutan@gmail.com>
2023-11-20 22:09:20 +0100

    Deleted word from badwords array

--- events/message/messageUpdate.js
+++ events/message/messageUpdate.js
@@ -131,7 +131,7 @@ module.exports = {
             "5h1t", "5hit", "a55", "anal", "anus", "ar5e", "arrse", "arse", "ass",
             "ass-fucker", "asses", "assfucker", "assfukka", "asshole", "assholes",
             "asswhole", "a_s_s", "b!tch", "b00bs", "b17ch", "b1tch", "ballbag",
-            "balls", "ballsack", "bastard", "beastial", "beastiality", "bellend",
+            "ballsack", "bastard", "beastial", "beastiality", "bellend",
             "bestial", "bestiality", "bi+ch", "biatch", "bitch", "bitcher", "bitchers",
             "bitches", "bitchin", "bitching", "bloody", "blow job", "blowjob", "blowjobs", 
             "boiolas", "bollock", "bollok", "boner", "boob", "boobs", "booobs", "boooobs", 
