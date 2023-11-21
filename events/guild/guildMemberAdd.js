151ba95c8de352eebaad49eca64e954fe116ec1e
Michał Kulik <michoorangutan@gmail.com>
2023-11-21 21:09:08 +0100

    Changed colors of welcome channel embeds

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -7,7 +7,7 @@ module.exports = {
         const welcomeChannel = member.guild.channels.cache.get('1159608455125020742');
 
         const welcomeEmbed = new EmbedBuilder()
-            .setColor('#00ff00')
+            .setColor('#424cdb')
             .setAuthor({name: 'User Joined!', iconURL: member.user.displayAvatarURL()})
             .setDescription(`Welcome <@${member.id}> to **${guild.name}**.\nWe now have [\`${guild.memberCount}\`] members!`)
             .setTimestamp();
--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -7,7 +7,7 @@ module.exports = {
         const welcomeChannel = member.guild.channels.cache.get('1159608455125020742');
 
         const byeEmbed = new EmbedBuilder()
-            .setColor('#ff0000')
+            .setColor('#cc3131')
             .setAuthor({name: 'User Left!', iconURL: member.user.displayAvatarURL()})
             .setDescription(`<@${member.id}> left.\nWe now have [\`${guild.memberCount}\`] members!`)
             .setTimestamp();
