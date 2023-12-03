4159d69059dfe7a43a4a2802c023d679a959873d
Michał Kulik <michoorangutan@gmail.com>
2023-12-03 01:28:30 +0100

    Added cooldown to messageCreate event + new message statement

--- events/message/messageCreate.js
+++ events/message/messageCreate.js
@@ -1,12 +1,32 @@
 const { Client } = require('discord.js');
 
+const cooldowns = new Map();
+
 module.exports = {
     name: 'messageCreate',
     on: true,
     execute(message) {
-        const messageChannel = message.guild.channels.cache.get('1170073523801301002');
-        if (message.content === "tak") {
-            messageChannel.send("nie");
+        const messageChannel = message.guild.channels.cache.get('1173943621179953253');
+
+        if (message.author.bot) {
+            return;
         }
+
+        if (cooldowns.has(message.author.id)) {
+            const expirationTime = cooldowns.get(message.author.id);
+            
+            if (Date.now() < expirationTime) {
+                return;
+            }
+        }
+
+        const cooldownTime = 3 * 60 * 1000;
+        cooldowns.set(message.author.id, Date.now() + cooldownTime);
+
+        if (message.content.toLowerCase() === "tak") {
+            messageChannel.send("nie");
+        } else if (message.content.toLowerCase() === "nie") {
+            messageChannel.send("tak");
+        } 
     }
 }
