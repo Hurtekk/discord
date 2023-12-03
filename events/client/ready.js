7c74fba9f05eee44dcad07406925511721c1389e
Michał Kulik <michoorangutan@gmail.com>
2023-12-03 01:46:25 +0100

    Added custom activity

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
-const { Client } = require('discord.js');
+const { Client, ActivityType } = require('discord.js');
 const mongoose = require('mongoose');
 const config = require('../../config.json');
 
@@ -14,5 +14,10 @@ module.exports = {
         }
 
         console.log(`${client.user.tag} is now online.`);
+        client.user.setActivity({ 
+            name: "Minevalik",
+            type: ActivityType.Watching, 
+            url: 'https://www.youtube.com/channel/UCOQYccq6nFPCdliE12Ux1cg',
+         });
     }
 }
